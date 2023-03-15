5d5099e0220fd3e22ea3b27dc110640c585c2605
facundo lezcano <faculezcano2@gmail.com>
2023-03-15 20:30:07 -0300

    resolucion del ejercicio

--- game.py
+++ game.py
@@ -7,6 +7,9 @@ times = 5
 # Contador inicial de tiempo.
 # Esto toma la fecha y hora actual.
 init_time = datetime.now()
+#inicio contadores
+cant_correctos = 0
+cant_incorrectos = 0
 print(f"¡Veremos cuanto tardas en responder estas {times} operaciones!")
 for i in range(0, times):
     # Se eligen números y operador al azar
@@ -16,11 +19,27 @@ for i in range(0, times):
     # Se imprime la cuenta.
     print(f"{i+1}- ¿Cuánto es {number_1} {operator} {number_2}?")
     # Le pedimos al usuario el resultado
-    result = input("resultado: ")
+    result = int(input("resultado: "))
+    #verifico el operador para realizar la cuenta
+    match operator:
+        case "+":
+           result_computer = number_1 + number_2
+        case "-":
+            result_computer = number_1 - number_2
+        case "*":
+            result_computer = number_1 * number_2
+        case "/":
+            result_computer = number_1 / number_2
+     
+    print(result_computer == result)
+    if(result_computer == result):
+        cant_correctos += 1
+    else:
+        cant_incorrectos += 1
 # Al terminar toda la cantidad de cuentas por resolver.
 # Se vuelve a tomar la fecha y la hora.
 end_time = datetime.now()
 # Restando las fechas obtenemos el tiempo transcurrido.
 total_time = end_time - init_time
 # Mostramos ese tiempo en segundos.
-print(f"\n Tardaste {total_time.seconds} segundos.")
+print(f"\n Tardaste {total_time.seconds} segundos. Cantidad de aciertos: {cant_correctos} , cantidad de incorrectos: {cant_incorrectos}")
